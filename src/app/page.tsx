8b6f506f1d9a7fc1931a626bd35d1a0e5067eede
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-05 22:05:43 +0300

    Add ProductsShowcase component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Hero from '@/components/Hero';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
+import ProductsShowcase from '../components/ProductsShowcase';
 
 export default function HomePage() {
   return (
@@ -17,6 +18,7 @@ export default function HomePage() {
         {/* כאן יתווספו הקומפוננטות שייווצרו על ידי המחולל */}
         <HeroSection />
     <AboutSection />
+    <ProductsShowcase />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
